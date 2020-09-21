5c358d05540b7a5f53ed0a817d86bb9a44a95ace
Anna Gingle <48488699+AnnaGingle@users.noreply.github.com>
2020-09-21 10:37:31 -0500

    Add files via upload

---
+++ ElixirLab/ConsoleApp1/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Item
+{
+	public int Ammount { get; set; }
+
+    private string name;
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetName(string value)
+    {
+        name = value;
+    }
+
+    public string Unit { get; set; }
+
+
+
+	public Item()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+		
+	}
+
+	public Item(string name, string unit, int ammount)
+    {
+        SetName(name);
+		Unit = unit;
+        Ammount = ammount;
+    }
+}
---
+++ ElixirLab/ConsoleApp1/Lab.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Lab
+{
+	public Lab()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
